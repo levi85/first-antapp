6e99ab48a6a1075b0a4238c1d8d74c8106f4c633
levi85 <joshua.zhangzy@gmail.com>
2018-07-10 22:49:49 +0800

    layout example

--- src/App.js
+++ src/App.js
@@ -4,29 +4,33 @@ import { DatePicker } from 'antd';
 import { Rate } from 'antd';
 import { TimePicker } from 'antd';
 import moment from 'moment';
+import { SliderDemo } from './SliderDemo';
 
 import './App.css';
 import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    testFun();
 
-        <div>
-        <DatePicker />
-        <TimePicker onChange={onChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
-        <Rate />
-      </div>
-        
-      </div>
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+
+      //   <div>
+      //   <DatePicker />
+      //   <TimePicker onChange={onChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+      //   <Rate />
+      // </div>
+      
+      // </div>
+      <SliderDemo />
     );
   }
 
@@ -34,6 +38,12 @@ class App extends Component {
 
 function onChange(time, timeString) {
   console.log(time, timeString);
+}
+
+function testFun() {
+
+ var test = {"a": "apple", "b": "banana"}; 
+  console.log("helloworld" + Object.keys(test));
 }
 
 export default App;
